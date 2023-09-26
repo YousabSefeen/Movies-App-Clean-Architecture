20725f1fdafc7c01896e1b59327f29a5f3667247
YousabSefeen <jesusyousab@gmail.com>
2023-09-26 20:51:34 +0200

    Improved the BlockConsumer location

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_movies_app/core/common%20presentation/splash/screens/splash_screen.dart';
 import 'package:flutter_movies_app/core/utils/app_routers.dart';
 
+import 'core/common presentation/widgets/custom_app_alerts.dart';
+import 'core/common presentation/widgets/no_internet_connection_screen.dart';
 import 'core/services/bloc_observer.dart';
 import 'core/services/services_locator.dart';
 import 'core/utils/app setting/controller/app_setting_cubit.dart';
@@ -29,38 +33,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => servicesLocator<AppSettingCubit>()
-        ..getThemePref()
-        ..checkInternetConnection(),
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => servicesLocator<AppSettingCubit>()
-              ..getThemePref()
-              ..checkInternetConnection(),
-          ),
-          BlocProvider(
-            create: (context) => servicesLocator<MoviesCubit>()
-              ..getNowPlayingMovies()
-              ..getPopularMovies()
-              ..getTopRatedMovies()
-              ..getUpcomingMovies(),
-          ),
-        ],
-        child: BlocBuilder<AppSettingCubit, AppSettingStates>(
-          builder: (context, state) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: AppStrings.movies,
-              theme: AppThemes.light(context),
-              darkTheme: AppThemes.dark(context),
-              themeMode: AppSettingCubit.object(context).theme,
-              home: const SplashScreen(),
-              routes: AppRouters.routers,
-            );
-          },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => servicesLocator<AppSettingCubit>()
+            ..getThemePref()
+            ..checkInternetConnection(),
         ),
+        BlocProvider(
+          create: (context) => servicesLocator<MoviesCubit>()
+            ..getNowPlayingMovies()
+            ..getPopularMovies()
+            ..getTopRatedMovies()
+            ..getUpcomingMovies(),
+        ),
+      ],
+      child: BlocConsumer<AppSettingCubit, AppSettingStates>(
+        listener: (context, state) {
+          if (state is NoInternetConnectionState) {
+            CustomAppAlerts.alertNoInternet(
+              context: context,
+              message: 'Could Not Connected',
+            );
+
+            Timer(
+              const Duration(seconds: 4),
+              () => AppRouters.goAndRemoveUntil(
+                context: context,
+                route: NoInternetScreen.route,
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: AppStrings.movies,
+            theme: AppThemes.light(context),
+            darkTheme: AppThemes.dark(context),
+            themeMode: AppSettingCubit.object(context).theme,
+            home: const SplashScreen(),
+            routes: AppRouters.routers,
+          );
+        },
       ),
     );
   }
